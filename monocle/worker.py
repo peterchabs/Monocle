38400670442b9480ef9289b0cb05716be1b68cd9
José Eduardo Gaytán Bustamante <jose.gaytan@outlook.com>
2017-04-24 13:05:20 -0700

    CP only for level 30+

--- monocle/worker.py
+++ monocle/worker.py
@@ -799,7 +799,7 @@ class Worker:
                             self.log.warning('{} during encounter', e.__class__.__name__)
 
                 if notify_conf and self.notifier.eligible(normalized):
-                    if encounter_conf and 'move_1' not in normalized:
+                    if encounter_conf and 'move_1' not in normalized and self.player_level >= 25:
                         try:
                             await self.encounter(normalized, pokemon.spawn_point_id)
                         except CancelledError:
